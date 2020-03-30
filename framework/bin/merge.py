0ecdb159f6ab299f49fb6775ec7a1565c0e0ce52
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2020-03-30 19:09:42 +0000

    fixed merge

--- framework/bin/merge.py
+++ framework/bin/merge.py
@@ -3,14 +3,14 @@
 import os,sys
 
 num_args = len(sys.argv)
-run_dir = os.getcwd()
-workflow_stage = sys.argv[1]
-filter_var = str(sys.argv[2])
+out_dir = sys.argv[1]
+workflow_stage = sys.argv[2]
+filter_var = str(sys.argv[3])
 
-out_file = "%s/results_%d_%s.txt" % (run_dir,workflow_stage,filter_var)
+out_file = "%s/results_%d_%s.txt" % (out_dir,workflow_stage,filter_var)
 
 with open(out_file,'w') as output:
-    for i in range(3,num_args):
+    for i in range(4,num_args):
         with open(str(sys.argv[i]),'r') as in_file:
             for line in in_file:
                 output.write(line)
