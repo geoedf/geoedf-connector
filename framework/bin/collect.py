aa5cff49579c4e2b6b3b92bc3d032accc13d9e9a
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2020-04-06 18:41:36 +0000

    Fixed collector script

--- framework/bin/collect.py
+++ framework/bin/collect.py
@@ -10,6 +10,6 @@ out_file = "%s/results_%s.txt" % (run_dir,workflow_stage)
 
 with open(out_file,'w') as output:
     for file_or_dir in os.listdir(target_dir):
-        full_path = '%s/%s\n' % (target_dir,file_or_dir)
+        full_path = '%s/%s' % (target_dir,file_or_dir)
         if os.path.isfile(full_path):
-            output.write(full_path)
+            output.write('%s\n' % full_path)
